a39d416aea896888a2d47593c15f6a3841b50d31
Ngowda94 <Nagraj94>
2022-07-12 08:57:31 +0530

    created simple linked list

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class LinkedList {
+    Node head;
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        /*
+        assigning values dynamically
+         */
+        list.head = new Node(56);
+        Node second = new Node(30);
+        Node third = new Node(70);
+        /*
+        pointing nodes to further nodes
+         */
+        list.head.next = second;
+        second.next = third;
+
+    }
+}
---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class Node {
+/*
+assigning variable
+ */
+    int data;
+    Node next;
+    /*
+    created constructor for node class
+     */
+    Node(int a){
+        data = a;
+        next = null;
+    }
+}
